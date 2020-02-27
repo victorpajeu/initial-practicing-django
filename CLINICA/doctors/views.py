12872c9dab985d73620e55c6c302fe6536764650
victorhugobezerramoreirapajeu <pajeuvictor@gmail.com>
2020-02-27 15:44:16 -0300

    Update 03

--- CLINICA/doctors/views.py
+++ CLINICA/doctors/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 from .models import Doctor
 from .forms import DoctorForm
@@ -12,4 +14,10 @@ class DoctorList(TemplateView):
         context['form'] = DoctorForm(self.request.POST or None)
         return context
 
-    
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        form = context['form']
+        if form.is_valid():
+            form.save()
+            return redirect(reverse_lazy('list'))
+
