7aa8fc55081025cff87c2c959d764d213226649c
victorhugobezerramoreirapajeu <pajeuvictor@gmail.com>
2020-02-27 13:52:57 -0300

    Criação de modelo de Doctors

--- CLINICA/doctors/models.py
+++ CLINICA/doctors/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Doctor(models.Model):
+    name = models.CharField('Nome', max_length=100)
+    age = models.IntegerField('Idade', null=False)
+    crm = models.IntegerField('CRM', null=False)
+    create_at = models.DateTimeField('Data e tempo de Criação', auto_now_add=True)
+    update_at = models.DateTimeField('Data e tempo de alteração', auto_now=True)
+
+    def __str__(self):
+        return self.name
