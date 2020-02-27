c422c1b090d8ed52638db69b7150b1b25d6e849c
victorhugobezerramoreirapajeu <pajeuvictor@gmail.com>
2020-02-27 14:18:06 -0300

    Plugs app in settins of projects, add abstract model Person

--- CLINICA/commons/models.py
+++ CLINICA/commons/models.py
@@ -5,3 +5,6 @@ class Person(models.Model):
     name = models.CharField("Nome", max_length=100)
     age = models.IntegerField("Idade", max_length=50)
     cpf = models.IntegerField("CPF", max_length=11)
+
+    class Meta:
+        abstract = True
