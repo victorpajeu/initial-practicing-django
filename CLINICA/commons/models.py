f36d2ec5401ce99b392040571fb274c32890bac0
victorhugobezerramoreirapajeu <pajeuvictor@gmail.com>
2020-02-27 15:12:55 -0300

    Update in project

--- CLINICA/commons/models.py
+++ CLINICA/commons/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Person(models.Model):
     name = models.CharField("Nome", max_length=100)
-    age = models.IntegerField("Idade", max_length=50)
-    cpf = models.IntegerField("CPF", max_length=11)
+    age = models.IntegerField("Idade")
+    cpf = models.IntegerField("CPF")
 
     class Meta:
         abstract = True
--- CLINICA/doctors/views.py
+++ CLINICA/doctors/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Doctor
 
-# Create your views here.
+
+class DoctorList(TemplateView):
+    template_name = 'doctors/doctor_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['doctors'] = Doctor.objects.all()
+        context['form'] =
