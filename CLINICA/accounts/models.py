897ee5944e7113d1986063d86af2ceac606e59e0
victorhugobezerramoreirapajeu <pajeuvictor@gmail.com>
2020-02-27 14:25:56 -0300

    Account model

--- CLINICA/accounts/models.py
+++ CLINICA/accounts/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Meta:
+        verbose_name = "Usuário"
+        verbose_name_plural = "Usuários"
+    REQUIRED_FIELDS = ('first_name',)
+
